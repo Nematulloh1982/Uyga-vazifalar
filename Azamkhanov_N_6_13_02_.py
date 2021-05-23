8bf1e6599cb34939c9a00861276b2c2278776e65
Nematilla Azamkhanov <84650662+Nematulloh1982@users.noreply.github.com>
2021-05-23 06:37:59 +0300

    Add files via upload

---
+++ Azamkhanov_N_6_13_02_.py
@@ -0,0 +1,30 @@
+# #6-dars, 1-topshiriq
+# #Muallif: Azamkhanov_N 
+
+# son=int(input("Istalgan son kiriting \n>>> "))
+# print (son,"ning kvadrati",son**2,"ga teng")
+# print (son,"ning kubi",son**3,"ga teng")
+
+#2-topshiriq
+# son=int(input("Yoshingizni kiritig \n>>> "))
+# print ("Siz",2021-son,"da tug'ilgansiz")
+
+
+# 3-topshiriq
+# print (son1,"-",son2,"=",son1-son2,)
+# print (son1,"*",son2,"=",son1*son2,)
+# print (son1,"/",son2,"=",son1/son2,)
+
+
+
+#Diskriminant
+a=float(input("a ni kirining "))
+b=float(input("b ni kirining "))
+c=float(input("c ni kirining "))
+d=(b**2)-(4*a*c)
+x1=(-b-(d**(1/2)))/(2*a)
+x2=(-b+(d**(1/2)))/(2*a)
+print('D=',d)
+print("x1=",x1)
+print("x2=",x2)
+
