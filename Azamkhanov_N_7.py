1b4c800e8b971a9319ebf7dc618ad599e4fb41e2
Nematilla Azamkhanov <84650662+Nematulloh1982@users.noreply.github.com>
2021-05-23 07:13:36 +0300

    Add files via upload

---
+++ Azamkhanov_N_7.py
@@ -0,0 +1,56 @@
+
+# Muallif:Azamhanov Nematulla
+#Amaliyot_7
+ismlar=['Ibroxim', "Sardor", "Abror"]
+print("Salom",ismlar[0],", bugun choyhona bormi?")
+print(ismlar[1],"choyhonaga boramizmi?")
+print("Qalaysan",ismlar[2],", soat nechada yuramiz?")
+
+sonlar=[546, -23, 81.759, -45.008]
+print(sonlar)
+sonlar.remove(-23)
+print(sonlar)
+sonlar.append(-207)
+print(sonlar)
+sonlar.insert(-1,73)
+print(sonlar)
+print(sonlar[0]-407, sonlar[1]+42.87)
+
+t_shaxslar=["Maxmud Qashqariy", "Imom Buhoriy", "At Termiziy", "Bahauddin Naqshbandiy"]
+z_shaxslar=["Abror Muhtor Ali", "Abdulloh domla", "Ersin Amir", "Rizabek Battaluli"]
+t_t=t_shaxslar.pop(0)
+z_z=z_shaxslar.pop(0)
+print("Men tarixiy shaxslardan", t_t, "bilan, zamonaviy shaxslardan esa", z_z, "bilan suxbat qilishni istar edim")
+
+friends=[]
+friends.append("Ibroxim")
+friends.append("Sardor")
+friends.append("Abror")
+friends.append("Izzatilla")
+friends.append("Yaxyo")
+friends.append("Suxrob")
+print(friends)
+friends.remove("Izzatilla")
+friends.remove("Suxrob")
+print(friends)
+friends.insert(0,"Ibodulla")
+friends.insert(3,"Anvar")
+friends.append("Oybek")
+print(friends)
+mexmonlar=[]
+mexmonlar.append(friends.pop(1))
+mexmonlar.append(friends.pop(1))
+mexmonlar.append(friends.pop(-3))
+mexmonlar.append(friends.pop(-2))
+print(mexmonlar)
+
+
+
+
+
+
+
+
+
+
+
