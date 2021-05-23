18af2dc75429444ab3fef68dc1bcc17fe877211a
Nematilla Azamkhanov <nematulloh1982@gmail.com>
2021-05-23 11:39:07 +0300

    Create 00002.py

---
+++ 00002.py
@@ -0,0 +1,9 @@
+# Muallif:Azamhanov Nematulla
+#masala_3.2 
+#13.02.2021
+print('"Nexia", "Tico",  \'Damas\' ko\'rganlar qilar havas')
+print("5 ning 4-darajasi" ,5**4)
+print("22 ni 4 ga bo'lganda", 22%4 , "qoldiq chiqadi")
+print ('S=',125*125, "P=",125*4)
+print("Diametri 12 ga teng bo'lgan doiraning yuzi",1/4*3.14*12**2)
+print("katetlari 6 va 7 bo'lgan to'g'ri burchakli uchburchakning gipotinuzasi",(6**2+7**2)**(1/2), "ga teng") 
